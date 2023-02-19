1c42979ecc47e9d84d1555d0f2207795e5a4a37d
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-02-19 15:49:36 -0500

    chore: update config files
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,32 +9,8 @@
  * @const config - ESLint configuration object
  */
 const config = {
-  root: true,
   extends: ['./.eslintrc.base.cjs'],
-  overrides: [
-    ...require('./.eslintrc.base.cjs').overrides,
-    {
-      files: [
-        './src/internal/err-invalid-arg-type.ts',
-        './src/internal/node-error.ts'
-      ],
-      rules: {
-        'unicorn/custom-error-definition': 0
-      }
-    },
-    {
-      files: ['./src/lib/format.ts', './src/lib/resolve.ts'],
-      rules: {
-        '@typescript-eslint/prefer-nullish-coalescing': 0
-      }
-    },
-    {
-      files: ['./src/export-regex.ts'],
-      rules: {
-        '@typescript-eslint/unbound-method': 0
-      }
-    }
-  ]
+  root: true
 }
 
 module.exports = config
